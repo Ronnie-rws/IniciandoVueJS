5db908cb10b60291805407adacac3dc3e04c0a9d
RonnieWillyamsRws <ronnie.willyams.rws@gmail.com>
2022-06-24 17:27:05 -0300

    Iniciando.
    Iniciando os estudos em Vue.js

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="app">
+     <h1>Guia de cliente</h1>
+     
+     
+   </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+ 
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,16 @@
+<template>
+   <h2>Cliente aqui</h2> 
+   <h3>Descrição do cliente</h3>
+</template>
+
+<script>
+
+export default{
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
